38e8c6b9e7ee65ae8a9569a7d6faa5e5d1064d2c
Viktor Zimmermann <viktorzimmermann11@web.de>
2018-09-25 16:03:42 +0200

    some files modified

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,20 @@
  **/
 
 import '../scss/mein.scss';
+import $ from 'jquery';
+import 'bootstrap';
+
+
+$(document).ready(function(){
+    if ($) {
+        // jQuery is loaded
+        console.log("Yeah!");
+    } else {
+        // jQuery is not loaded
+        console.log("Doesn't Work");
+    }
+});
+
+/////////////////////////////////////////////////////////////////////////////
+
 console.log("hi! Распиздяй!");
